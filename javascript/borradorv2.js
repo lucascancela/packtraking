7fb8c1b07d6f8631a0cd9bc0275328039cd9d658
lucascancela <luccancela@gmail.com>
2022-09-21 20:37:42 -0300

    Update borradorv2.js

--- javascript/borradorv2.js
+++ javascript/borradorv2.js
@@ -1,24 +1,40 @@
 var bloques = [ 
+    [1,0,0,0,0,0],
     [0,0,0,0,0,0],
+    [0,0,3,0,0,0],
     [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
+    [0,0,0,0,5,0],
 ]
     
 //dia = fila 
 function solve(horarios){
     let nuevoHorarios = [...horarios];
+
     nuevoHorarios.forEach(dia => {
     });
+    console.log(nuevoHorarios[1]) //jugar con esto
 
     bloques_invertido = invertir(nuevoHorarios);
         //ver video del negro de backtracking
     bloques_invertido.forEach(columna => {
          
     });
+
+    let valor;
+
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 6; j++) {
+            if(nuevoHorarios[i][j] == 0){
+            nuevoHorarios[i][j] = j
+        }
+        
+        console.log(nuevoHorarios)
+        
+    }
+    }
 }
 solve(bloques);
+
 function invertir(bloques){
     lista_bloques = [];
     for (let i = 0; i < 6; i++) {
