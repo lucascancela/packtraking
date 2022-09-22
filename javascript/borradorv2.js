c5224a02c510dbe434835a97916c941d5faf76e9
lucascancela <luccancela@gmail.com>
2022-09-22 06:18:50 -0300

    aaaa me estoy quemando

--- javascript/borradorv2.js
+++ javascript/borradorv2.js
@@ -1,37 +1,44 @@
 var bloques = [ 
     [1,0,0,0,0,0],
     [0,0,0,0,0,0],
-    [0,0,3,0,0,0],
+    [0,0,0,3,0,0],
     [0,0,0,0,0,0],
     [0,0,0,0,5,0],
 ]
-    
+    console.log("hola")
 //dia = fila 
 function solve(horarios){
-    let nuevoHorarios = [...horarios];
+  //  let nuevoHorarios2 = [...nuevoHorarios];
 
-    nuevoHorarios.forEach(dia => {
-    });
-    console.log(nuevoHorarios[1]) //jugar con esto
+    // nuevoHorarios.forEach(dia => {
+    // });
+    
+    // bloques_invertido = invertir(nuevoHorarios);
+    //     //ver video del negro de backtracking
+    // bloques_invertido.forEach(columna => {
+        // });
+        
+   console.log(horarios[2][3]) //jugar con esto
 
-    bloques_invertido = invertir(nuevoHorarios);
-        //ver video del negro de backtracking
-    bloques_invertido.forEach(columna => {
-         
-    });
+   for (let i = 0; i < 5; i++) {
+       let nuevoHorarios2
+       let valor=1
 
-    let valor;
+        for (let j = 0; j < 6;j++) {
+            if(horarios[i][j] == 0){
+            horarios[i][j] = valor
+            valor
+            solve(horarios)
+            
+            }            
+            valor++
 
-    for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < 6; j++) {
-            if(nuevoHorarios[i][j] == 0){
-            nuevoHorarios[i][j] = j
         }
         
-        console.log(nuevoHorarios)
+        console.log(horarios)
         
     }
-    }
+    
 }
 solve(bloques);
 
@@ -53,4 +60,4 @@ function invertir(bloques){
     return lista_bloques;
 }
 
-console.log(invertir(bloques));
+//console.log(invertir(bloques));
