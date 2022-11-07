e6e470f1e606d2d7683501d10c0d026cca5ce18b
lucascancela <luccancela@gmail.com>
2022-11-07 17:07:10 -0300

    ,jb,j

--- javascript/borradorv3.js
+++ javascript/borradorv3.js
@@ -15,25 +15,38 @@ var materias = ["lengua"]
 function solve(horarios, valor){        
     console.log(horarios[2][3]) //jugar con esto
     valor
+    let num = 0;
+    let newValor;
+    newValor = valor;
     for (let i = 0; i < 5; i++) {
-        let newValor;
-        newValor = valor;
-
+        
         for (let j = 0; j < 6;j++) {
-            if(horarios[i][j] == 0){ // aca tengo q poner las condiciones para ver si se pone algo ahi o se saltea
+            horarios[i][j] = newValor
+            horarios
+            for (let k = 0; k < 5; k++) {
+                for (let l = 0; l < 6; l++) {
+                    horarios
+                    
+                    if(horarios[k][l]==0){
+                      //  solve(horarios, newValor++)
+                    }                    
+                }
+            }
+          //  solve(horarios, newValor++)            
+            
+            // aca tengo q poner las condiciones para ver si se pone algo ahi o se saltea
             
             //qeu se reemplaze le lugar por un numero, sino q siga al else
-
-            //horarios[i][j] = newValor
-           // solve(horarios, valor);
-            }else{
-                //que vuelva uno atras y le sume uno al numero de atras solo si no estaba ocupado antes por un numero puesto de base
-                //revisar lo de arriba porque no es sudoku, es horarios
-            }
-            //   if horarios is a solution
-
             
-           // newValor++
+            //horarios[i][j] = newValor
+            // solve(horarios, valor);
+            //que vuelva uno atras y le sume uno al numero de atras solo si no estaba ocupado antes por un numero puesto de base
+            //revisar lo de arriba porque no es sudoku, es horarios
+            
+            //   if horarios is a solution
+            
+            
+            // newValor++
         }
         
         console.log(horarios)
