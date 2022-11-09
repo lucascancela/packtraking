b33b9b7506e8f0bea9413f5164237fa838da8b7c
lucascancela <luccancela@gmail.com>
2022-11-09 10:44:02 -0300

    ikbk

--- javascript/borradorv4.js
+++ javascript/borradorv4.js
@@ -2,17 +2,30 @@ var bloques = [0,0,0,0,0,0];
 
 var materias = ["mate", "mate", "lengua", "lengua","proy", "proy"];
 
-var profeMate =   [1,0,1,1,0,0];
-var profeLengua = [0,1,0,1,1,1]; 
-var profeProy =   [1,1,1,0,0,1];
+var profeMate =   [1,1,1,1,1,1];
+var profeLengua = [1,1,1,1,1,1]; 
+var profeProy =   [1,1,1,1,1,1];
 
 function solve(horarios, materias){
-    for (let i = 0; i < materias.length; i++) { //REVISAR EL I++ VER BORRADORV5 COMO HICE
+    let num=0;
+    for (let i = 0; materias.length > 0; i++) { //REVISAR EL I++ VER BORRADORV5 COMO HICE
+        horarios
         if(horarios[i]==0){
-            //if() se puede poner la materia segun el horario del profe
-            horarios[i] = materias[0];
-            materias.splice(0,1);
-            horarios
+            console.log(materias[0])
+            console.log(profeMate[i])
+            materias
+
+            for (let j = 0; j <= materias.length; j++) {
+                if(materias[j]=="mate" && profeMate[i]==1 || materias[j]=="proy" && profeProy[i]==1 || materias[j]=="lengua" && profeLengua[i]==1){
+                    //   solve(horarios,materias);
+                    horarios[i] = materias[j];
+                    materias.splice(j,1);
+                    break;
+                }
+            }
+            
+          //  horarios[i] = materias[0];
+            //   solve(horarios,materias);
         }
     }
 }
--- javascript/borradorv5.js
+++ javascript/borradorv5.js
@@ -1,14 +1,20 @@
 var bloques = [0,0,0,0,0,0];
-var materias = ["mate", "mate", "lengua", "lengua","proy", "proy"];
-
+var materias = ["mate", "lengua", "proy"];
+var num1 =289380;
+var num2 = 0;
 for (let i = 0; i < materias.length; i++) {
     if(bloques[i]==0){
         
     }    
 }
+materias.splice(2,1)
+console.log(materias)
 for (let i = 0; i < materias.length;) {
     materias.splice(0,1)
     materias
+}
+if(num1 == 289380 || num2==1){
+    console.log("hola")
 }
 console.log(materias)
 bloques[0] = materias[0]
@@ -16,6 +22,7 @@ console.log(bloques);
 
 
 function solve(bloques, materias){
-
+console.log("hola")
 
 }
+solve(bloques, materias)
