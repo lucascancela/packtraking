76ed0efcf3f9f1fced1f4046fdad4ddf4241886c
lucascancela <luccancela@gmail.com>
2022-11-17 17:24:18 -0300

    Update tuto.js

--- javascript/tuto.js
+++ javascript/tuto.js
@@ -1,16 +1,4 @@
-const numeros  = [4, 2, 1, 3, , 4, 2];
-let duplicados = [];
- 
-const tempArray = [...numeros].sort();
- 
-for (let i = 0; i < tempArray.length; i++) {
-  console.log(tempArray);
-  console.log(i);
-  console.log(tempArray[i]);
-
-  if (tempArray[i + 1] === tempArray[i]) {
-    duplicados.push(tempArray[i]);
-  }
-}
- 
-console.log(duplicados); // [2, 4]
+var hola = Math.random()*10;
+hola
+hola=Math.round(hola)
+hola
