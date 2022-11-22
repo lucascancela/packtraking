850693b62d0613398aa1f8942271e64a121e4199
lucascancela <luccancela@gmail.com>
2022-11-22 15:15:38 -0300

    jjh

--- javascript/borradorv4.js
+++ javascript/borradorv4.js
@@ -9,12 +9,7 @@ var profeProy =   [0,0,0,1,1,1];
 function solve(horarios, materias){
     let num=0;
     for (let i = 0; materias.length > 0; i++) { //REVISAR EL I++ VER BORRADORV5 COMO HICE
-        horarios
         if(horarios[i]==0){
-            console.log(materias[0])
-            console.log(profeMate[i])
-            materias
-
             for (let j = 0; j < materias.length; j++) {
                 if(materias[j]=="mate" && profeMate[i]==1 || materias[j]=="proy" && profeProy[i]==1 || materias[j]=="lengua" && profeLengua[i]==1){
                     //   solve(horarios,materias);
@@ -24,8 +19,6 @@ function solve(horarios, materias){
                 }
             }
             bloques
-            
-          //  horarios[i] = materias[0];
             //   solve(horarios,materias);
         }
     }
